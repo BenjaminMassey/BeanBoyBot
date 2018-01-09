ea90d8e3b4fee3845e0a1ee5df80001ca624c008
BenjaminMassey <benjamin.w.massey@gmail.com>
2018-01-08 16:10:24 -0800

    Slightly fixed !flex

--- TwitchChat.java
+++ TwitchChat.java
@@ -82,14 +82,6 @@ public class TwitchChat extends PircBot {
 					+ "you can do, and have fun!");
 		}
 		
-		if (message.equalsIgnoreCase("!flex")) {
-			if(PlayersHandler.getLeaderBoard().contains(sender))
-				messageChat("Damn, " + sender + " has " + PlayersHandler.getPoints(sender)
-						+ " points! PogChamp");
-			else
-				privateMessage(sender, "Pfff not even on the leaderboard smh");
-		}
-		
 		if (message.equalsIgnoreCase("!join")) {
 			boolean joined = PlayersHandler.addPlayer(sender);
 			if (joined)
@@ -273,6 +265,14 @@ public class TwitchChat extends PircBot {
 			
 			if(message.equalsIgnoreCase("!leaderboard"))
 				privateMessage(sender, PlayersHandler.getLeaderBoard());
+			
+			if (message.equalsIgnoreCase("!flex")) {
+				if(PlayersHandler.getLeaderBoard().toLowerCase().contains(sender.toLowerCase()))
+					messageChat("Damn, " + sender + " has " + PlayersHandler.getPoints(sender)
+							+ " points! PogChamp");
+				else
+					privateMessage(sender, "Pfff not even on the leaderboard smh");
+			}
 		}
 	}
 	
