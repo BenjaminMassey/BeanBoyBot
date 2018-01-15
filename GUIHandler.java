c0e86db2061b47a941a29af4220245a405080aa4
BenjaminMassey <benjamin.w.massey@gmail.com>
2018-01-14 17:24:50 -0800

    Updated GUI to be able to toggle config settings - very poorly coded and a little sloppy, but it works and will be an important feature that will allow for even more options

--- GUIHandler.java
+++ GUIHandler.java
@@ -12,94 +12,41 @@ public class GUIHandler extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public static JLabel cost;
+	private static GUIHandler frame;
+	private static JPanel configPanel;
+	private static JPanel nonConfigPanel;
+	private static JPanel main;
+	private static JButton startButtonConfig;
+	private static JButton startButtonNonConfig;
 	
 	public static void createWindow(String name, String icon) {
-		//Create and set up the window.
-        GUIHandler frame = new GUIHandler(name);
+		// Create and set up the window
+        frame = new GUIHandler(name);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Display the window.
+        // Display the window
         ImageIcon ico = new ImageIcon(icon);
         frame.setIconImage(ico.getImage());
         frame.pack();
         frame.setVisible(true);
+        frame.setSize(250,150);
 	}
 	
 	public GUIHandler(String name) {
         super(name);
-        JPanel jp = new JPanel();
-        jp.setLayout(new GridLayout(9,1));
-        jp.add(new JLabel("                BeanBoyBot Twitch Bot                ", SwingConstants.CENTER));
         
-        JTextField chatChannel = new JTextField(20);
-        if(!AccountsManager.getChatChannel().substring(1).equals("ailed D:"))
-        	chatChannel.setText(AccountsManager.getChatChannel().substring(1));
-        jp.add(chatChannel);
-        JButton channelButton = new JButton("Set Chat Channel");
-        jp.add(channelButton);
-        channelButton.addActionListener(new ActionListener(){
-        	public void actionPerformed(ActionEvent ae) {
-        		AccountsManager.setChatChannel(chatChannel.getText());
-        	}
-        });
-        JTextField botName = new JTextField(20);
-        if(!AccountsManager.getBotName().equals("Failed D:"))
-        	botName.setText(AccountsManager.getBotName());
-        jp.add(botName);
-        JButton botNameButton = new JButton("Set Bot Name");
-        jp.add(botNameButton);
-        botNameButton.addActionListener(new ActionListener(){
-        	public void actionPerformed(ActionEvent ae) {
-        		AccountsManager.setBotName(botName.getText());
-        	}
-        });
-        JTextField botOauth = new JTextField(20);
-        if(!AccountsManager.getBotOauth().equals("Failed D:"))
-        	botOauth.setText("****************");
-        jp.add(botOauth);
-        JButton botOauthButton = new JButton("Set Bot Oauth");
-        jp.add(botOauthButton);
-        botOauthButton.addActionListener(new ActionListener(){
-        	public void actionPerformed(ActionEvent ae) {
-        		AccountsManager.setBotOauth(botOauth.getText());
-        		botOauth.setText("****************");
-        	}
-        });
-        /*
-        cost = new JLabel("", SwingConstants.CENTER);
-        jp.add(cost);
-        */
-        JLabel blank = new JLabel("");
-        jp.add(blank);
-        JButton startButton = new JButton("Start");
-        jp.add(startButton);
-        startButton.addActionListener(new ActionListener(){
-        	public void actionPerformed(ActionEvent ae) {
-        		if(TwitchChat.connected) {
-        			try{
-        				TwitchChat.deactivate();
-        				PlayersHandler.saveAll();
-        			}catch(Exception e) {
-        				System.err.println("Oops: " + e);
-        			}
-        			startButton.setText("Start");
-	        		pack();
-        		}
-        		else {
-        			try{
-        				AccountsManager.updateAll();
-        				LiveSplitHandler.initialize();
-        				SplitGame.start();
-        				TwitchChat.initialize();
-        			}catch(Exception e) {
-        				System.err.println("Oops: " + e);
-        			}
-        			startButton.setText("Stop");
-        			pack();
-        		}
-        	}
-        });
-        add(jp);
+        nonConfigPanel = generateNonConfigPanel();
+        configPanel = generateConfigPanel();
+        
+        main = new JPanel();
+        main.setLayout(new CardLayout());
+        
+        main.add(nonConfigPanel);
+        main.add(configPanel);
+        
+        add(main);
+        
+        
+        // Handle exit
         addWindowListener(new WindowListener() {
         	public void windowClosing(WindowEvent we) {
         		System.exit(1);
@@ -120,6 +67,140 @@ public class GUIHandler extends JFrame {
         });
     }
 	
+	private static JButton generateStartButton() {
+		JButton startButton = new JButton("Start");
+        startButton.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent ae) {
+        		if(TwitchChat.connected) {
+        			try{
+        				TwitchChat.deactivate();
+        				PlayersHandler.saveAll();
+        			}catch(Exception e) {
+        				System.err.println("Oops: " + e);
+        			}
+        			startButtonConfig.setText("Start");
+        			startButtonNonConfig.setText("Start");
+        		}
+        		else {
+        			try{
+        				AccountsManager.updateAll();
+        				LiveSplitHandler.initialize();
+        				SplitGame.start();
+        				TwitchChat.initialize();
+        			}catch(Exception e) {
+        				System.err.println("Oops: " + e);
+        			}
+        			startButtonConfig.setText("Stop");
+        			startButtonNonConfig.setText("Stop");
+        		}
+        	}
+        });
+        return startButton;
+	}
 	
+	private static JPanel generateConfigPanel() {
+		
+		JPanel jp = new JPanel();
+		jp.setLayout(new GridLayout(10,1));
+        
+        // Put on a title label
+        jp.add(new JLabel("                BeanBoyBot Twitch Bot                ", SwingConstants.CENTER));
+        
+        // Button to toggle configuration
+        JButton configButton = new JButton("Hide config");
+        jp.add(configButton);
+        configButton.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent ae) {
+        		CardLayout cl = (CardLayout) main.getLayout();
+        		cl.next(main);
+        		frame.setSize(250,150);
+        	}
+        });
+		
+		// Entry for what channel the bot should be in
+        JTextField chatChannel = new JTextField(20);
+        if(!AccountsManager.getChatChannel().substring(1).equals("ailed D:"))
+        	chatChannel.setText(AccountsManager.getChatChannel().substring(1));
+        jp.add(chatChannel);
+        // Button to confirm channel
+        JButton channelButton = new JButton("Set Chat Channel");
+        jp.add(channelButton);
+        channelButton.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent ae) {
+        		AccountsManager.setChatChannel(chatChannel.getText());
+        	}
+        });
+        
+        // Entry for the bot's name
+        JTextField botName = new JTextField(20);
+        if(!AccountsManager.getBotName().equals("Failed D:"))
+        	botName.setText(AccountsManager.getBotName());
+        jp.add(botName);
+        // Button to confirm bot's name
+        JButton botNameButton = new JButton("Set Bot Name");
+        jp.add(botNameButton);
+        botNameButton.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent ae) {
+        		AccountsManager.setBotName(botName.getText());
+        	}
+        });
+        
+        // Entry for bot oauth code (will be obscured very simply)
+        JTextField botOauth = new JTextField(20);
+        if(!AccountsManager.getBotOauth().equals("Failed D:"))
+        	botOauth.setText("****************");
+        jp.add(botOauth);
+        // Button to confirm bot's oauth code
+        JButton botOauthButton = new JButton("Set Bot Oauth");
+        jp.add(botOauthButton);
+        botOauthButton.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent ae) {
+        		AccountsManager.setBotOauth(botOauth.getText());
+        		botOauth.setText("****************");
+        	}
+        });
+        
+        // Blank space for spacing
+        JLabel blank = new JLabel("");
+        jp.add(blank);
+        
+        // Button that toggles the bot on and off
+        startButtonConfig = generateStartButton();
+        jp.add(startButtonConfig);
+        
+        return jp;
+        
+	}
+	
+	private static JPanel generateNonConfigPanel() {
+		
+		JPanel jp = new JPanel();
+		jp.setLayout(new GridLayout(4,1));
+        
+        // Put on a title label
+        jp.add(new JLabel("                BeanBoyBot Twitch Bot                ", SwingConstants.CENTER));
+        
+        // Button to toggle configuration
+        JButton configButton = new JButton("Unhide config");
+        jp.add(configButton);
+        configButton.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent ae) {
+        		CardLayout cl = (CardLayout) main.getLayout();
+        		cl.next(main);
+        		frame.setSize(250,300);
+        	}
+        });
+        
+        // Blank space for spacing
+        JLabel blank = new JLabel("");
+        jp.add(blank);
+        
+        // Button that toggles the bot on and off
+        startButtonNonConfig = generateStartButton();
+        jp.add(startButtonNonConfig);
+        
+        return jp;
+        
+	}
 	
 }
